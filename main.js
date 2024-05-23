a234b95068be21299a8e558de70989d0ad94f6dc
Emiliapk <emiliapenak@gmail.com>
2024-05-23 17:37:05 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function cambiar(){
+    var etiqueta=document.getElementById(etiqueta).value;
+
+if(etiqueta=='Hola mundo'){
+   document.getElementById(etiqueta).value("Adios mundo");
+}else{ 
+    document.getElementById(etiqueta).value("Hola mundo");
+
+}
+}
